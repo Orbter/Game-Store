13a9f78afa30466197cfba533fea3184fdf6892f
Orbter <assouline39@gmail.com>
2024-11-06 00:48:28 +0200

    made an arrow to the slider

--- src/layout/MainContainer.jsx
+++ src/layout/MainContainer.jsx
@@ -2,10 +2,14 @@ import { MainSlider } from '../components/mainSlider/MainSlider';
 import { InformationSlider } from '../components/mainSlider/informationSlider';
 import useEmblaCarousel from 'embla-carousel-react';
 import mainPhoto from '../assets/main-image.png';
+import { useCallback } from 'react';
+import arrow from '../assets/svg/arrow.svg';
 
 function MainContainer() {
   const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true });
-
+  function cheking() {
+    console.log('work');
+  }
   const slidesData = [
     {
       title: 'God of War Ragnarok',
@@ -23,24 +27,45 @@ function MainContainer() {
       image: mainPhoto,
     },
   ];
+  const scrollPrev = useCallback(() => {
+    if (emblaApi) emblaApi.scrollPrev();
+  }, [emblaApi]);
+
+  const scrollNext = useCallback(() => {
+    if (emblaApi) emblaApi.scrollNext();
+  }, [emblaApi]);
 
   return (
-    <div
-      className="bg-main-blue w-9/12  md:w-10/12 h-[fit-content] cursor-pointer embla "
-      ref={emblaRef}
-    >
-      <div className="embla__container">
-        {slidesData.map((slide, index) => (
-          <div key={index} className="embla__slide md:flex">
-            <div className="w-full md:w-[60vw]">
-              <MainSlider image={slide.image} />
-            </div>
-            <div className="w-full md:w-[40vw]">
-              <InformationSlider />
-            </div>
+    <div className='w-full flex justify-center h-[fit-content] items-center '>
+      <button
+        className='embla__prev h-[fit-content] md:pr-5'
+        onClick={scrollPrev}
+      >
+        <img src={arrow}></img>
+      </button>
+      <div className='bg-main-blue w-9/12  md:w-10/12 h-[fit-content] cursor-pointer embla '>
+        <div className='embla-viewport' onClick={cheking} ref={emblaRef}>
+          <div className='embla__container'>
+            {slidesData.map((slide, index) => (
+              <div key={index} className='embla__slide md:flex'>
+                <div className='w-full md:w-[60vw]'>
+                  <MainSlider image={slide.image} />
+                </div>
+                <div className='w-full md:w-[40vw]'>
+                  <InformationSlider />
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
+        </div>
       </div>
+
+      <button
+        className='embla__next h-[fit-content] flex  md:pl-5'
+        onClick={scrollNext}
+      >
+        <img src={arrow} className='rotate-180'></img>
+      </button>
     </div>
   );
 }
