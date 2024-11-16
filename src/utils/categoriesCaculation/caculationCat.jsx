b35529638c2e01fe01b289bd46c83176fd327c46
Orbter <assouline39@gmail.com>
2024-11-16 18:51:15 +0200

    fixed the bugs in the calculation

--- src/utils/categoriesCaculation/caculationCat.jsx
+++ src/utils/categoriesCaculation/caculationCat.jsx
@@ -2,32 +2,44 @@ function addingClasses(allCategories, cssClasses) {
   const cssClassesCurrent = cssClasses;
   const randomClasses = (array) => {
     let chooseClass = false;
+    let color;
     while (!chooseClass) {
-      const randomNumber = Math.floor(Math.random() * 7);
+      const randomNumber = Math.floor(Math.random() * array.length);
       if (array[randomNumber].counter !== 0) {
         array[randomNumber].counter -= 1;
         chooseClass = true;
+        color = array[randomNumber].color;
       }
-      return array[randomNumber].color;
     }
+    return color;
   };
-  allCategories.map((item) => ({
+  const updatedCategories = allCategories.map((item) => ({
     ...item,
     randomClasses: randomClasses(cssClassesCurrent),
   }));
+  return updatedCategories;
 }
 // randomize the sliders so that every time it will have 4 different card every time tou open the website
 
 function randomSlider(allCategories) {
   let listOver = false;
+  const copyAllCategories = [...allCategories];
   const newAllCategories = [];
   while (!listOver) {
     let list = new Set();
     while (list.size < 4) {
-      const randomNumber = Math.floor(Math.random() * allCategories.length);
-      list.add(allCategories[randomNumber]);
+      const randomNumber = Math.floor(Math.random() * copyAllCategories.length);
+      let number;
+      if (randomNumber === copyAllCategories.length) {
+        copyAllCategories.pop();
+      } else {
+        number = copyAllCategories.splice(randomNumber, 1);
+      }
+      const removeItem = number.pop();
+      list.add(removeItem);
     }
     newAllCategories.push([...list]);
+    console.log(newAllCategories);
     list = new Set();
     newAllCategories.length === 3 && (listOver = true);
   }
@@ -35,3 +47,5 @@ function randomSlider(allCategories) {
 }
 
 export { addingClasses, randomSlider };
+
+//
